ff33a3b79cf88292d44d3b211fe64a5e139c08d0
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-30 20:09:07 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id, INT 
+);
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(department_name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO roles(title, department_id, salary)
+VALUES ('Sales Lead', 1, 100000), ('Saleperson', 1, 80000), ('Lead Engineer', 2, 150000), ('Software Engineer', 2, 120000), ('Account Manager', 3, 160000), ('Accountant', 3, 125000), ('Legal Team Lead', 4, 250000), ('Lawyer', 4, 190000);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Aaron', 'Torres', 1, )
