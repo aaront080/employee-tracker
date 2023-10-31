c1a132563c6e38e6a5bc6e1ba9babaf527262d19
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-30 20:17:08 -0700

    query department function added

---
+++ index.js
@@ -0,0 +1,65 @@
+const inquirer = require('inquirer');
+const console = require('console.table');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: '1350Southh90!',
+    database: 'employee_db'
+});
+const PORT = 3001
+
+connection.connect(err => {
+    if (err) throw (err);
+});
+
+const userOptions = () =>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'Options',
+            message: 'Please make a selection.',
+            choices: ['view all departments',
+                    'view all roles',
+                    'view all employees',
+                    'add a department',
+                    'add a role',
+                    'add an employee',
+                    'update an employee role',
+        ]
+        }
+    ])
+    .then((responses) => {
+        const { choices } = responses;
+        if (choices === 'view all departments'){
+            queryDepartments();
+        }
+        if (choices === 'view all roles'){
+            queryRoles();
+        }
+        if (choices === 'view all employees'){
+            queryEmployees();
+        }
+        if (choices === 'add a department'){
+            addDepartment();
+        }
+        if (choices === 'add a role'){
+            addRole();
+        }
+        if (choices === 'add an employee'){
+            addEmployee();
+        }
+    })
+}
+
+queryDepartments = () => {
+    console.log('Now viewing all departments:\n');
+    const sql = `SELECT department.id AS id, department.department_name AS department FROM department`;
+    connection.query(sql, (err, response) => {
+        if (err) throw err;
+        console.table(response);
+        userOptions();
+    })
+};
